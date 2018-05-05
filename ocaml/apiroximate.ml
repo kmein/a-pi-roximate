a39662c096f7b3269be7640ffad4953ca7390b65
kmein <kieran.meinhardt@gmail.com>
2018-05-05 18:12:27 +0200

    OCaml: Use complex numbers as points

--- ocaml/apiroximate.ml
+++ ocaml/apiroximate.ml
@@ -1,13 +1,11 @@
-type point = { x : float; y : float }
-
-let dist p = hypot p.x p.y
+open Complex
 
 let approximation count =
-    let random () = {x = Random.float 1.; y = Random.float 1.} in
+    let random () = {re = Random.float 1.; im = Random.float 1.} in
     let rec randoms = function
         | 0 -> []
         | n -> random () :: randoms (n - 1) in
-    let in_circle = List.length (List.filter (fun p -> dist p <= 1.) (randoms count)) in
+    let in_circle = List.length (List.filter (fun p -> norm p <= 1.) (randoms count)) in
     4.0 *. float in_circle /. float count;;
 
 for count = 1 to 100000 do
