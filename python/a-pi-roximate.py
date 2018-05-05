62828f6b83c93f757c5819a9c8324f42e2d10084
kmein <kieran.meinhardt@gmail.com>
2018-05-05 18:14:52 +0200

    Python: Use complex numbers as points

--- python/a-pi-roximate.py
+++ python/a-pi-roximate.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 from random import random
-from math import sqrt
-
-def distance(point):
-    x, y = point
-    return sqrt(x**2 + y**2)
 
 def approximation(count):
     def random_points(count):
         for _ in range(count - 1):
-            yield random(), random()
-    in_circle = len(list((p for p in random_points(count) if distance(p) <= 1)))
+            yield complex(random(), random())
+    in_circle = len(list((p for p in random_points(count) if abs(p) <= 1)))
     return 4 * (in_circle / float(count))
 
 for count in range(1, 100000):
