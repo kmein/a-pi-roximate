e3d91f1bc12553b3f930bd164180a98090afa6b8
kmein <kieran.meinhardt@gmail.com>
2018-05-05 18:04:20 +0200

    C: Use complex numbers as points

--- c/apiroximate.c
+++ c/apiroximate.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
+#include <complex.h>
 
-struct point {
-    double x;
-    double y;
-};
-
-double distance(const struct point *p) {
-    return sqrt(pow(p->x, 2) + pow(p->y, 2));
+double complex random_point() {
+    double x = (double)rand() / RAND_MAX;
+    double y = (double)rand() / RAND_MAX;
+    return x + y * I;
 }
 
-struct point random_point() {
-    struct point p;
-    p.x = (double)rand() / RAND_MAX;
-    p.y = (double)rand() / RAND_MAX;
-    return p;
-}
-
-struct point *random_points(unsigned int count) {
-    struct point *result = malloc(count * sizeof(struct point));
+double complex *random_points(unsigned int count) {
+    double complex *result = malloc(count * sizeof(double complex));
     for (int i = 0; i < count; i++) {
         result[i] = random_point();
     }
@@ -29,13 +19,13 @@ struct point *random_points(unsigned int count) {
 
 double approximate(unsigned int count) {
     unsigned int in_circle = 0;
-    struct point *points = random_points(count);
+    double complex *points = random_points(count);
     for (int i = 0; i < count; i++) {
-        if (distance(&points[i]) <= 1) {
+        if (cabs(points[i]) <= 1) {
             in_circle++;
         }
     }
-    return 4 * (float)in_circle / count;
+    return 4 * (double)in_circle / count;
 }
 
 int main() {
