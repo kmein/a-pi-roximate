f8fda3414c3593405be6381668ea704ffcdb7017
kmein <kieran.meinhardt@gmail.com>
2018-05-05 18:09:44 +0200

    Haskell: Use complex numbers as points

--- haskell/apiroximate.hs
+++ haskell/apiroximate.hs
@@ -1,18 +1,16 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-11.1 --install-ghc runghc --package random
+{-# LANGUAGE TypeApplications #-}
+import Data.Complex
 import Control.Monad (replicateM, forM_)
 import System.Random (randomIO)
 import Text.Printf (printf)
 
-data Point = Point Double Double
-
-dist (Point x y) = sqrt (x^2 + y^2)
-
 approximate :: Int -> IO Double
 approximate count = do
-    let randomPoint = Point <$> randomIO <*> randomIO
+    let randomPoint = (:+) <$> randomIO <*> randomIO @Double
         randomPoints = flip replicateM randomPoint
-    inCircle <- (length . filter (\p -> dist p <= 1)) <$> randomPoints count
+    inCircle <- (length . filter (\z -> magnitude z <= 1)) <$> randomPoints count
     pure $ 4 * (fromIntegral inCircle / fromIntegral count)
 
 main = forM_ [1 .. 100000] $ \n -> printf "%6d %f\n" n =<< approximate n
